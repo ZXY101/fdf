7fb5657dbc2cc55e739a83a1d567202bb466e43e
Shaun TENNER <stenner@c4r21s4.wethinkcode.co.za>
2019-06-29 16:39:41 +0200

    Coll stuff

--- testing/test.c
+++ testing/test.c
@@ -6,11 +6,19 @@
 /*   By: stenner <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/28 16:29:55 by stenner           #+#    #+#             */
-/*   Updated: 2019/06/29 16:14:59 by stenner          ###   ########.fr       */
+/*   Updated: 2019/06/29 16:38:13 by stenner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fdf.h"
+
+int rgb(int r, int g, int b)
+{
+	int rgb;
+
+	rgb = 65536 * r + 256 * g + b;
+	return rgb;
+}
 
 int test(int key, void *param)
 {
@@ -26,9 +34,13 @@ int test2(int button, int x, int y, void *param)
 	void **point = (void**)param;
 	if (button == 1)
 		//mlx_string_put(point[0],point[1], x, y, 255);
-		mlx_pixel_put(point[0], point[1], x, y, 255);
+		mlx_string_put(point[0], point[1], x, y, 2672885, "A");
+	else if (button == 2)
+		mlx_string_put(point[0], point[1], x, y, 16711680, "B");
+	else if (button == 3)
+		mlx_string_put(point[0], point[1], x, y, 65280, "C");
 	else
-		mlx_pixel_put(point[0], point[1], x, y, 16711680);
+		mlx_pixel_put(point[0], point[1], x, y, 16777215);
 	return (0);
 }
 
@@ -42,7 +54,7 @@ int		main(void)
 	win_ptr = mlx_new_window(mlx_ptr, 600, 600, "Test");
 	params[0] = mlx_ptr;
 	params[1] = win_ptr;
-	mlx_string_put(mlx_ptr, win_ptr, 300, 300, 16777215, "Hello World!");
+	mlx_string_put(mlx_ptr, win_ptr, 250, 300, rgb(255, 255, 0), "Hello World!");
 	mlx_key_hook(win_ptr, test,(void *)0);
 	mlx_mouse_hook (win_ptr, test2, (void *)params);
 	mlx_loop(mlx_ptr);
