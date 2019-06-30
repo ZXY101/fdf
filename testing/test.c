d68eff4d1a50cbfb4361fefa8bc17e53fc5a618d
ZXY101 <xzxy101x@gmail.com>
2019-06-30 23:47:55 +0200

    Lines?

--- testing/test.c
+++ testing/test.c
@@ -6,23 +6,68 @@
 /*   By: stenner <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/28 16:29:55 by stenner           #+#    #+#             */
-/*   Updated: 2019/06/29 16:38:13 by stenner          ###   ########.fr       */
+/*   Updated: 2019/06/30 23:36:22 by stenner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fdf.h"
+#include <stdio.h>
+#include <math.h>
 
 int rgb(int r, int g, int b)
 {
 	int rgb;
+	if (r > 255)
+		r = 255;
+	if (g > 255)
+		g = 255;
+	if (b > 255)
+		b = 255;
 
 	rgb = 65536 * r + 256 * g + b;
 	return rgb;
 }
 
+struct Coord
+{
+		int x;
+		int y;
+};
+
+int draw_line(struct Coord c1, struct Coord c2,	void *mlx_ptr, void *win_ptr)
+{
+	int deltaX;
+	int deltaY;
+	int i;
+	int j;
+	int e;
+
+	deltaX = c2.x - c1.x;
+	deltaY = c2.y - c1.y;
+	j = c1.y;
+	i = c1.x;
+	if (deltaX < 0 && deltaY < 0)
+		e = deltaY - deltaX;
+	else
+		e = deltaY / deltaY;
+	while (i < c2.x)
+	{
+		mlx_pixel_put(mlx_ptr, win_ptr, i, j, rgb(0,0,255));
+		if (e > 0)
+		{
+			j++;
+			e -= deltaX;
+		}
+		i++;
+		e+= deltaY;
+	}
+	return (0);
+}
+
 int test(int key, void *param)
 {
-	if (key == 53)
+	(void)param;
+	if (key == 0xff1b)
 	{
 		exit(0);
 	}
@@ -33,7 +78,6 @@ int test2(int button, int x, int y, void *param)
 {
 	void **point = (void**)param;
 	if (button == 1)
-		//mlx_string_put(point[0],point[1], x, y, 255);
 		mlx_string_put(point[0], point[1], x, y, 2672885, "A");
 	else if (button == 2)
 		mlx_string_put(point[0], point[1], x, y, 16711680, "B");
@@ -52,9 +96,16 @@ int		main(void)
 
 	mlx_ptr = mlx_init();
 	win_ptr = mlx_new_window(mlx_ptr, 600, 600, "Test");
+	struct Coord c1;
+	struct Coord c2;
+	c1.x = 10;
+	c1.y = 10;
+	c2.x = 100;
+	c2.y = 10;
+	draw_line(c1, c2, mlx_ptr, win_ptr);
 	params[0] = mlx_ptr;
 	params[1] = win_ptr;
-	mlx_string_put(mlx_ptr, win_ptr, 250, 300, rgb(255, 255, 0), "Hello World!");
+	//mlx_string_put(mlx_ptr, win_ptr, 250, 300, rgb(255, 255, 0), "Hello World!");
 	mlx_key_hook(win_ptr, test,(void *)0);
 	mlx_mouse_hook (win_ptr, test2, (void *)params);
 	mlx_loop(mlx_ptr);
