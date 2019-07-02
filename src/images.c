fea03fbafaefb426fd60961757d6cc6190334add
Shaun TENNER <stenner@c4r23s5.wethinkcode.co.za>
2019-07-02 13:48:54 +0200

    yes

--- src/images.c
+++ src/images.c
@@ -6,30 +6,30 @@
 /*   By: stenner <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/02 12:45:17 by stenner           #+#    #+#             */
-/*   Updated: 2019/07/02 12:51:07 by stenner          ###   ########.fr       */
+/*   Updated: 2019/07/02 13:23:22 by stenner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fdf.h"
-#include <stdio.h>
-void pixel_put_image(t_mlx_image *img, int colour, int x, int y)
+
+void	pixel_put_image(t_mlx_image *img, int colour, int x, int y)
 {
 	int *data;
 
 	if (x >= img->width || x < 0)
-		return;
+		return ;
 	if (y >= img->height || y < 0)
-		return;
+		return ;
 	data = (int*)&img->raw_data[x * 4 + y * img->size_line];
-
 	*data = colour;
 }
 
-void clear_image(t_mlx_image *img, int colour)
+void	clear_image(t_mlx_image *img, int colour)
 {
-	int *data = (int*)img->raw_data;
+	int *data;
 	int i;
 
+	data = (int*)img->raw_data;
 	i = 0;
 	while (i < (img->width * img->height))
 	{
@@ -38,7 +38,7 @@ void clear_image(t_mlx_image *img, int colour)
 	}
 }
 
-void init_image(t_environment *env, t_mlx_image *img, int width, int height)
+void	init_image(t_environment *env, t_mlx_image *img, int width, int height)
 {
 	img->img_ptr = mlx_new_image(env->mlx_ptr, width, height);
 	img->raw_data = mlx_get_data_addr(img->img_ptr, &img->bpp, &img->size_line,
@@ -47,12 +47,10 @@ void init_image(t_environment *env, t_mlx_image *img, int width, int height)
 	img->height = height;
 	img->pos.x = 0;
 	img->pos.y = 0;
-	printf("bbp: %d\nwidth: %d\nheight: %d\nsize_line: %d\nendian: %d\nx: %d\ny: %d\n",
-	img->bpp,img->width,img->height,img->size_line,img->endian,img->pos.x,img->pos.y);
 	clear_image(img, 0x000000ff);
 }
 
-void put_image(t_environment *env, t_mlx_image *img)
+void	put_image(t_environment *env, t_mlx_image *img)
 {
 	mlx_put_image_to_window(env->mlx_ptr, env->win_ptr, img->img_ptr,
 	img->pos.x, img->pos.y);
